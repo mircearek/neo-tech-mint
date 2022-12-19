af604925a1fb51cf136523bac4807b2c1764c197
ruaneotech <contact@neotech.finance>
2022-12-19 21:18:45 +0200

    syntax error fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Banner from "../components/banner";
 import Footer from "../components/footer";
 import HowToMint from "../components/how-to-mint";
-import HowToMint from "../components/rubic";
+import Rubic from "../components/rubic";
 import Mint from "../components/mint";
 
 export default function Home() {
