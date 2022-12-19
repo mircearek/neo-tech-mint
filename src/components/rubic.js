f3f5b382e4dc2d7bcd969b263f981fef1958c77f
ruaneotech <contact@neotech.finance>
2022-12-19 21:29:47 +0200

    tried new fix without helmet

--- src/components/rubic.js
+++ src/components/rubic.js
@@ -4,7 +4,35 @@ import { Col, Container, Row } from 'react-bootstrap'
 
 
 export default function Rubic() {
+    var configuration = {
+        from: 'USDT',
+        to: 'TRX',
+        fromChain: 'ETH',
+        toChain: 'TRON',
+        amount: 1,
+        iframe: 'vertical',
+        hideSelectionFrom: false,
+        hideSelectionTo: true,
+        tokenSearch: true,
+        rubicLink: true,
+        theme: 'dark',
+        background: '#660f53',
+        injectTokens: {
+            'eth': [
+                '0x3330BFb7332cA23cd071631837dC289B09C33333'
+            ]
+        },
+        slippagePercent: {
+            instantTrades: 2,
+            crossChain: 5
+        }
+    }
+
+    Object.freeze(configuration);
+
+    rubicWidget.init(configuration);
     return (
+        
         <div className='rubic'>
             <Container>
                 
@@ -14,39 +42,7 @@ export default function Rubic() {
                     </Col>
                     <Col lg={6} sm={12} md={6}>
                         <div id="rubic-widget-root"></div>
-                        <>
-                            <Helmet>
-                                <script type="text/javascript" defer>
-                                    var configuration = {
-                                        from: 'USDT',
-                                        to: 'TRX',
-                                        fromChain: 'ETH',
-                                        toChain: 'TRON',
-                                        amount: 1,
-                                        iframe: 'vertical',
-                                        hideSelectionFrom: false,
-                                        hideSelectionTo: true,
-                                        tokenSearch: true,
-                                        rubicLink: true,
-                                        theme: 'dark',
-                                        background: '#660f53',
-                                        injectTokens: {
-                                            'eth': [
-                                                '0x3330BFb7332cA23cd071631837dC289B09C33333'
-                                            ]
-                                        },
-                                        slippagePercent: {
-                                            instantTrades: 2,
-                                            crossChain: 5
-                                        }
-                                    }
-
-                                    Object.freeze(configuration);
-
-                                    rubicWidget.init(configuration);
-                                </script>
-                            </Helmet>
-                        </>
+                        
                     </Col>
                 </Row>
             </Container>
