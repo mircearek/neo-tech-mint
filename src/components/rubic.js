2df11835d32f535cbd3a3568c77a7ba88c818df2
ruaneotech <contact@neotech.finance>
2022-12-19 21:25:17 +0200

    moved rubic to helmet

--- src/components/rubic.js
+++ src/components/rubic.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from "react-helmet"
 import { Col, Container, Row } from 'react-bootstrap'
 
 
@@ -6,9 +7,46 @@ export default function Rubic() {
     return (
         <div className='rubic'>
             <Container>
+                
                 <Row>
                     <Col lg={6} sm={12} md={6}>
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/FYbtxt0KhIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </Col>
+                    <Col lg={6} sm={12} md={6}>
+                        <div id="rubic-widget-root"></div>
+                        <>
+                            <Helmet>
+                                <script type="text/javascript" defer>
+                                    var configuration = {
+                                        from: 'USDT',
+                                        to: 'TRX',
+                                        fromChain: 'ETH',
+                                        toChain: 'TRON',
+                                        amount: 1,
+                                        iframe: 'vertical',
+                                        hideSelectionFrom: false,
+                                        hideSelectionTo: true,
+                                        tokenSearch: true,
+                                        rubicLink: true,
+                                        theme: 'dark',
+                                        background: '#660f53',
+                                        injectTokens: {
+                                            'eth': [
+                                                '0x3330BFb7332cA23cd071631837dC289B09C33333'
+                                            ]
+                                        },
+                                        slippagePercent: {
+                                            instantTrades: 2,
+                                            crossChain: 5
+                                        }
+                                    }
+
+                                    Object.freeze(configuration);
+
+                                    rubicWidget.init(configuration);
+                                </script>
+                            </Helmet>
+                        </>
                     </Col>
                 </Row>
             </Container>
