b15dfaf754f1f8dcd8ce0e0f89c93f146e1623f3
ruaneotech <contact@neotech.finance>
2022-12-19 22:13:25 +0200

    rubic try

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,33 @@ import Rubic from "../components/rubic";
 import Mint from "../components/mint";
 
 export default function Home() {
+  const configuration = {
+    from : 'USDT',
+    to : 'TRX',
+    fromChain : 'ETH',
+    toChain: 'TRON',
+    amount: 1,
+    iframe: 'vertical',
+    hideSelectionFrom: false,
+    hideSelectionTo: true,
+    tokenSearch: true,
+    rubicLink: true,
+    theme: 'dark',
+    background: '#660f53',
+    injectTokens: {
+        'eth': [
+            '0x3330BFb7332cA23cd071631837dC289B09C33333'
+        ]
+    },
+    slippagePercent: {
+        instantTrades: 2,
+        crossChain: 5
+    }
+    }
+    
+    Object.freeze(configuration);
+    
+    rubicWidget.init(configuration);
   return (
     <div>
       <Banner />
